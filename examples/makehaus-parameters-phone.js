45eb24f3f51aa9635cd7e7bdbd6df74f7cd8fda5
rishabh <arora.rishabh1989@gmail.com>
2020-06-15 20:38:40 +0530

    Updated bind use case in parameters example. Requires a bugfix in parameters to function properly.

--- examples/makehaus-parameters-phone.js
+++ examples/makehaus-parameters-phone.js
@@ -6,27 +6,94 @@ const path = require('path');
 const jsonPath = path.join(__dirname, 'layouts/makehaus-parameters-phone.json');
 const layoutJson = fs.readFileSync(jsonPath);
 
-MakeHaus.init(
-  layoutJson,
-  () => {
-    /* UI Initialization was successful. The web app is now running */
-  },
-  () => {
-    const dynamicWidgetStackNames = [1, 2, 3, 4].map(id => 'stack-' + id);
-    const typeButtonStackNames = [5, 6, 7, 8].map(id => 'stack-' + id);
-    const bindButtonStacknames = [9, 10, 11, 12].map(id => 'stack-' + id);
+const animateBindsForward = ({ widgetStacks, typesStacks, bindStacks }) => {
+  const selectedColor = '#0000ff';
+  const unselectedColor = '#444444';
+  const parametersOfStack = widgetStacks.map(s => s.parameter());
+  const map = new Map();
+  const selectedMap = new Map();
 
-    const widgetStacks = Stacks.getAll().filter(stack => dynamicWidgetStackNames.includes(stack.name()));
-    const typesStacks = Stacks.getAll().filter(stack => typeButtonStackNames.includes(stack.name()));
-    const bindStacks = Stacks.getAll().filter(stack => bindButtonStacknames.includes(stack.name()));
+  bindStacks.forEach(s => {
+    selectedMap.set(s, false);
+    const mappedIdx =
+      s
+        .parameter()
+        .id()
+        .split('-')[1] % 9;
+    s.parameter().addListener(evt => {
+      if (evt.value) {
+        switch (evt.value) {
+          case '>':
+            toggleBind(s);
+            break;
+          default:
+            break;
+        }
+      }
+    });
+    map.set(s, mappedIdx);
+  });
 
-    animateBinds({ widgetStacks, typesStacks, bindStacks });
-  }
-);
+  const toggleBind = s => {
+    console.log(`toggling ${map.get(s)} of stack ${s.name()}`);
+    toggleSelected(s);
+    // parametersOfStack[mappedIdx].bindFrom(parametersOfStack[mappedIdx + 1], () => {});
+  };
 
-const animateBinds = ({ widgetStacks, typesStacks, bindStacks }) => {
-  /* simple animator function to bind two stacks (and internally their respective parameters)*/
+  const applyColor = s => {
+    selectedMap.get(s) === true ? s.setColor(selectedColor) : s.setColor(unselectedColor);
+  };
 
+  const updateParameterSelectedState = s => {
+    selectedMap.set(s, !selectedMap.get(s));
+  };
+
+  const handleColor = s => {
+    applyColor(s);
+  };
+
+  const bindAllToLeftToDestination = (destination, lefts, currIndex) => {
+    for (let i = currIndex - 1; i >= 0; i--) {
+      const ls = lefts[i];
+      if (selectedMap.get(ls) !== true) break;
+      const param = parametersOfStack[map.get(ls)];
+      param.unbind();
+      param.bindFrom(destination, () => {});
+    }
+  };
+
+  const handleSelected = s => {
+    //unbind self
+    const param = parametersOfStack[map.get(s)];
+    param.unbind();
+    //find next bind stack ahead of me which is unselected - the destination parameter
+    const rightOfMe = bindStacks.slice(bindStacks.indexOf(s) + 1);
+    const nextUnselectedStack = rightOfMe.filter(s => selectedMap.get(s) === false)[0];
+    //bind self to destination parameter
+    const mappedParamOfNextUnselectedStack = parametersOfStack[map.get(nextUnselectedStack)];
+    param.bindFrom(mappedParamOfNextUnselectedStack, () => {});
+    // bind all selected to the left of me to the destination parameter
+    const leftOfMe = bindStacks.slice(0, bindStacks.indexOf(s));
+    bindAllToLeftToDestination(mappedParamOfNextUnselectedStack, leftOfMe, bindStacks.indexOf(s));
+  };
+
+  const handleUnselected = s => {
+    // unbind self
+    const param = parametersOfStack[map.get(s)];
+    param.unbind();
+
+    const leftOfMe = bindStacks.slice(0, bindStacks.indexOf(s));
+    bindAllToLeftToDestination(param, leftOfMe, bindStacks.indexOf(s));
+  };
+
+  const toggleSelected = s => {
+    updateParameterSelectedState(s);
+    selectedMap.get(s) ? handleSelected(s) : handleUnselected(s);
+    handleColor(s);
+  };
+};
+
+const animateBindsCentralParam = ({ widgetStacks, typesStacks, bindStacks }) => {
   /* create the central parameter to which all the stacks will bind to*/
   const central = Parameters.newParameter('maker', 'central');
   central.updateType({ min: 0, max: 100, step: 1, value: 0, type: ParameterType.NUMBER });
@@ -86,3 +153,20 @@ const animateBinds = ({ widgetStacks, typesStacks, bindStacks }) => {
     parametersOfStack[idx].updateType(available[++i % available.length]);
   };
 };
+
+const runnable = animateBindsForward;
+MakeHaus.init(
+  layoutJson,
+  () => {},
+  () => {
+    /* UI Initialization was successful. The web app is now running */
+    const dynamicWidgetStackNames = [1, 2, 3, 4].map(id => 'stack-' + id);
+    const typeButtonStackNames = [5, 6, 7, 8].map(id => 'stack-' + id);
+    const bindButtonStacknames = [9, 10, 11, 12].map(id => 'stack-' + id);
+
+    const widgetStacks = Stacks.getAll().filter(stack => dynamicWidgetStackNames.includes(stack.name()));
+    const typesStacks = Stacks.getAll().filter(stack => typeButtonStackNames.includes(stack.name()));
+    const bindStacks = Stacks.getAll().filter(stack => bindButtonStacknames.includes(stack.name()));
+    runnable({ widgetStacks, typesStacks, bindStacks });
+  }
+);
